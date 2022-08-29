2036fab1a798bd68933e88d27b3f8a5b3f38b8c3
valentinaiori <valentinaiori@live.it>
2022-08-29 19:28:36 +0200

    milestone 3

--- script.js
+++ script.js
@@ -22,3 +22,61 @@ MILESTONE 3
 Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, 
 che quindi verrà visualizzata al posto della precedente.
 */
+
+const images = ['01','02', '03', '04','05'];
+
+// recuperare container delle immagini
+const container = document.getElementById('carousel-container');
+
+// container.innerHTML += '<img class="item active" src="img/01.jpg" alt="due"/>'
+
+
+for (let i = 0; i < images.length; i++){
+
+    const image = images[i];
+    container.innerHTML +=`<img class="item" src="img/${image}.jpg" alt='${image}'/>` 
+}
+
+const firstItem = document.querySelector('.item')
+firstItem.classList.add('active')
+
+
+
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+
+
+nextButton.addEventListener('click',
+    function(){
+
+
+        // recupero l'elemento attivo
+        const activeItem = document.querySelector('.item.active');
+
+        const nextElement = activeItem.nextElementSibling;
+
+
+        if(nextElement){
+            activeItem.classList.remove('active')
+            nextElement.classList.add('active');
+        }
+           
+    }       
+);
+
+previousButton.addEventListener('click',
+    function(){
+
+
+        // recupero l'elemento attivo
+        const activeItem = document.querySelector('.item.active');
+        
+        const previousElement = activeItem.previousElementSibling;
+
+        if(previousElement){
+            activeItem.classList.remove('active')
+            previousElement.classList.add('active');
+        }
+           
+    }       
+);
